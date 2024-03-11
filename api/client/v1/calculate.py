cafc1b7d6071348f0ade64e143ebc30d53c7ad5a
Maxkambek <mahkamkasimov94@gmail.com>
2024-03-11 13:04:59 +0500

    adawd

--- api/client/v1/calculate.py
+++ api/client/v1/calculate.py
@@ -4,10 +4,10 @@ from api.client.client_main.models import Order
 
 def calculate_distance(lat1, lon1, lat2, lon2):
     # Convert latitude and longitude to radians
-    lat1 = math.radians(lat1)
-    lon1 = math.radians(lon1)
-    lat2 = math.radians(lat2)
-    lon2 = math.radians(lon2)
+    lat1 = math.radians(float(lat1))
+    lon1 = math.radians(float(lon1))
+    lat2 = math.radians(float(lat2))
+    lon2 = math.radians(float(lon2))
 
     # Haversine formula
     dlon = lon2 - lon1
@@ -22,6 +22,7 @@ def calculate_distance(lat1, lon1, lat2, lon2):
 def filter_nearby_locations_order(user_latitude, user_longitude):
     nearby_locations = []
     for location in Order.objects.all():
+        print(location)
         distance = calculate_distance(user_latitude, user_longitude, location.latitude, location.longitude)
         if distance <= 100:
             nearby_locations.append(location)
